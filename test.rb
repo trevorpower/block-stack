031c24551779e896686d36fc4e35d1664bd85ae3
Trevor Power <trevor_power@yahoo.com>
2012-07-05 20:11:35 +0100

    use minitest for tests

--- test.rb
+++ test.rb
@@ -1,19 +1,34 @@
+require 'minitest/autorun'
 require './block_stack'
 
-stack = BlockStack.new
+class TestBlockStack < MiniTest::Unit::TestCase
+  def setup
+    @blocks = BlockStack.new
+  end
 
-simple = 'test'
+  def test_that_empty_stack_is_empty
+    assert_equal true, @blocks.empty?
+  end
 
-stack.push do |a|
-  simple + a + simple
+  def test_that_stack_is_not_empty_after_pushing_a_block
+    @blocks.push {}
+    assert_equal false, @blocks.empty?
+  end
+
+  def test_that_stack_chain_is_executed_correctly_wit_no_params
+    @blocks.push { 'middle' }
+    @blocks.push { "<#{peek}>" }
+    @blocks.push { "{#{peek!}}" }
+
+    assert_equal '{<middle>}', @blocks.peek
+  end
+
+  def test_that_stack_chain_is_executed_correctly_with_param
+    @blocks.push { |a| a }
+    @blocks.push { peek! }
+    @blocks.push { |a| "<#{peek a + 'dle'}>" }
+    @blocks.push { "{#{peek!}}" }
+
+    assert_equal '{<middle>}', @blocks.peek('mid')
+  end
 end
-
-stack.push do |*a|
-  "<#{peek!}>"
-end
-
-stack.push do |*a|
-  "{#{peek!}}"
-end
-
-puts stack.peek('==')
